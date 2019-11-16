ab35fe9652bee8a57cdaca32e8f30a0a8a2a847e
Aurelijus Banelis <aurelijus@banelis.lt>
2019-11-16 19:32:16 +0200

    Pridedame Sonata Admin bundle
    Kadangi patenka į TOP 100 labiausiai atsiunčiamų Symfony recype.
    
    Sugeneruota per scripts/backend.sh:
      composer require sonata-project/admin-bundle
    
    Dokumentacija:
      https://sonata-project.org/bundles/admin/3-x/doc/index.html
      https://packagist.org/packages/sonata-project/admin-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,10 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Sonata\DatagridBundle\SonataDatagridBundle::class => ['all' => true],
+    Sonata\CoreBundle\SonataCoreBundle::class => ['all' => true],
+    Sonata\BlockBundle\SonataBlockBundle::class => ['all' => true],
+    Knp\Bundle\MenuBundle\KnpMenuBundle::class => ['all' => true],
+    Sonata\AdminBundle\SonataAdminBundle::class => ['all' => true],
 ];
