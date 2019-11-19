901b4df4313697e8d2078e68f15a96586f189b8e
Migle <kr.migle@gmail.com>
2019-11-19 21:38:50 +0200

    hw try

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -28,9 +29,9 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(KernelInterface $request)
     {
-        $json = file_get_contents("students.json");
+        $json = file_get_contents($request->getProjectDir() . '/public/students.json');
         $data = json_decode($json, true);
         return $this->render('home/index.html.twig', [
             'students' => $this->groupByStudents($data),
